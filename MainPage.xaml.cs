537e7bc1c6edc593c6f1bbfd1b06bedbe7970dfc
SimphiweGithub <simphiwe0105@gmail.com>
2025-04-23 21:10:48 +0200

    started with ui and made some changes to android specifically

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator_App
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+
+}
---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Microsoft.Maui;
+
+namespace Calculator_App;
+
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (Handler, view) => 
+            {
+                if (view is Entry)
+                {
+                    Handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                    Handler.PlatformView.ShowSoftInputOnFocus = false;
+                }
+            });
+            
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+
